7a319632a3b78bdc8e6931e472fbd8555cb3e6ae
Maxime Bury <maxime.bury@gmail.com>
2014-05-02 00:14:55 -0400

    Add logging, timing handler

--- downsizr.go
+++ downsizr.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httptest"
+	"os"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var (
@@ -18,11 +25,52 @@ func init() {
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/", hello)
+	router := mux.NewRouter()
+	router.HandleFunc("/resize", downsize).Methods("POST")
+
+	timingHandler := TimingHandler(router)
+	loggingHandler := handlers.LoggingHandler(os.Stdout, timingHandler)
+
+	http.Handle("/", loggingHandler)
 	log.Println("Downsizr listening on port", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
+type Request struct {
+	SourceURL string
+	Sizes     []string
+}
+
+type Response struct {
+}
+
+func downsize(res http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var request Request
+	if err := decoder.Decode(&request); err != nil {
+		log.Println(err)
+		http.Error(res, "malformed request", http.StatusBadRequest)
+		return
+	}
+
 	fmt.Fprintln(res, "hello, world")
+}
+
+func TimingHandler(handler http.Handler) http.Handler {
+	timer := func(res http.ResponseWriter, req *http.Request) {
+		rec := httptest.NewRecorder()
+		now := time.Now()
+
+		handler.ServeHTTP(rec, req)
+
+		for k, v := range rec.Header() {
+			res.Header()[k] = v
+		}
+		res.Header().Set("X-Time-Elapsed", time.Since(now).String())
+
+		res.WriteHeader(rec.Code)
+		res.Write(rec.Body.Bytes())
+	}
+
+	return http.HandlerFunc(timer)
 }
